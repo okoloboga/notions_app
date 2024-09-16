a3b4d94bcdb2b864db1ad27cdf4688424eb9d138
user <okoloboga37@gamil.com>
2024-09-16 09:27:06 +0300

    Init Database, using env variables

--- api/database.py
+++ api/database.py
@@ -1,1 +1,21 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from config import get_config, BotConfig, DbConfig
+
+
+# Аддресс Базы Данных берём из переменных окружения
+db_config = get_config(DbConfig, "db")
+
+# На продакшене echo обысно отключаю, что бы логи были чище
+engine = create_async_engine(
+        url=str(db_config.dsn), 
+        echo=False)
+
+SessionLocal = sessionmaker(autocommit=False, 
+                            autoflush=False, 
+                            bind=engine, 
+                            class_=AsyncSession)
+
 
